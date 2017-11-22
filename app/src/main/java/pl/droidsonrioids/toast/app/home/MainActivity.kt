9a3ecedc5f2d26e262ccee380cb82b7175fa6d3d
nonda <nonda95@gmail.com>
2017-11-22 11:13:16 +0100

    Fixes after merge

--- app/src/main/java/pl/droidsonrioids/toast/app/home/MainActivity.kt
+++ app/src/main/java/pl/droidsonrioids/toast/app/home/MainActivity.kt
@@ -6,17 +6,32 @@ import kotlinx.android.synthetic.main.activity_main.*
 import pl.droidsonrioids.toast.R
 import pl.droidsonrioids.toast.app.base.BaseActivity
 
+const val EVENTS_TAB_INDEX = 0
+const val LECTURERS_TAB_INDEX = 1
+const val CONTACT_TAB_INDEX = 2
+
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupToolbar()
+        setupViewPager()
     }
 
     private fun setupToolbar() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    private fun setupViewPager() {
+        homeViewPager.adapter = HomePagerAdapter(supportFragmentManager)
+        with(homeTabLayout) {
+            setupWithViewPager(homeViewPager)
+            getTabAt(EVENTS_TAB_INDEX)?.setIcon(R.drawable.ic_tab_events_selector)
+            getTabAt(LECTURERS_TAB_INDEX)?.setIcon(R.drawable.ic_tab_lecturers_selector)
+            getTabAt(CONTACT_TAB_INDEX)?.setIcon(R.drawable.ic_tab_contact_selector)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
