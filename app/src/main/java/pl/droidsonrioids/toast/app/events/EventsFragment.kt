f9cd778c5d19cd1c5592532994d67bf6f0b1e81c
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2017-11-27 12:44:47 +0100

    [TOA-37] Code refactoring, Mockito installed

--- app/src/main/java/pl/droidsonrioids/toast/app/events/EventsFragment.kt
+++ app/src/main/java/pl/droidsonrioids/toast/app/events/EventsFragment.kt
@@ -1,9 +1,10 @@
-package pl.droidsonrioids.toast
+package pl.droidsonrioids.toast.app.events
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import pl.droidsonrioids.toast.R
 
 class EventsFragment : Fragment() {
 
