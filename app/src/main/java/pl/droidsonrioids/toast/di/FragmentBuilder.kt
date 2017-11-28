200612817c897e25fa2b22a1064f48a414d3a22f
nonda <nonda95@gmail.com>
2017-11-28 13:15:57 +0100

    Fixed initial scroll bug on events view Added blank speakers view

--- app/src/main/java/pl/droidsonrioids/toast/di/FragmentBuilder.kt
+++ app/src/main/java/pl/droidsonrioids/toast/di/FragmentBuilder.kt
@@ -2,10 +2,10 @@ package pl.droidsonrioids.toast.di
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import pl.droidsonrioids.toast.app.dummy.DummyFragment
+import pl.droidsonrioids.toast.app.speakers.SpeakersFragment
 
 @Module
 abstract class FragmentBuilder {
     @ContributesAndroidInjector
-    abstract fun bindDummyFragment(): DummyFragment
+    abstract fun bindSpeakersFragment(): SpeakersFragment
 }
