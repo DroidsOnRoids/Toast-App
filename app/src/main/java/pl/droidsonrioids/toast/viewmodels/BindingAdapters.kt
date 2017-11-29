da2578c1e193d7256015ab07d9116b029f17bae4
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2017-11-29 20:08:22 +0100

    [TOA-35][WIP]Event cover image implementation, androud support libraries version changed

--- app/src/main/java/pl/droidsonrioids/toast/viewmodels/BindingAdapters.kt
+++ app/src/main/java/pl/droidsonrioids/toast/viewmodels/BindingAdapters.kt
@@ -1,12 +1,16 @@
 package pl.droidsonrioids.toast.viewmodels
 
 import android.databinding.BindingAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
+import pl.droidsonrioids.toast.R
+import pl.droidsonrioids.toast.data.model.Image
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-object BindAdapters {
+object BindingAdapters {
 
     @JvmStatic
     @BindingAdapter("eventTime")
@@ -27,4 +31,16 @@ object BindAdapters {
             textView.text = formattedDate
         }
     }
+
+    @JvmStatic
+    @BindingAdapter("eventCoverImage")
+    fun setEventCoverImage(imageView: ImageView, imageUrlList: List<Image>?) {
+        if (imageUrlList != null)
+            Picasso.with(imageView.context)
+                    .load(imageUrlList[1].big)
+                    .placeholder(R.drawable.ic_placeholder_toast)
+                    .into(imageView)
+    }
+
+
 }
