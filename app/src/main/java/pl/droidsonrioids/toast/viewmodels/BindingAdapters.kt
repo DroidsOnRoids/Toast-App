abe1bbc93e89829389c2c7060b1929962809cc63
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2017-11-30 12:00:33 +0100

    [TOA-35]Event cover image implemented

--- app/src/main/java/pl/droidsonrioids/toast/viewmodels/BindingAdapters.kt
+++ app/src/main/java/pl/droidsonrioids/toast/viewmodels/BindingAdapters.kt
@@ -3,7 +3,9 @@ package pl.droidsonrioids.toast.viewmodels
 import android.databinding.BindingAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.jakewharton.picasso.OkHttp3Downloader
 import com.squareup.picasso.Picasso
+import okhttp3.OkHttpClient
 import pl.droidsonrioids.toast.R
 import pl.droidsonrioids.toast.data.model.Image
 import java.text.SimpleDateFormat
@@ -11,11 +13,14 @@ import java.util.*
 
 
 object BindingAdapters {
+    private const val TIME_PATTERN = "hh:mm a zz"
+    private const val DATE_PATTERN = "dd.M.Y"
+    private const val FIRST_COVER_INDEX = 0
 
     @JvmStatic
     @BindingAdapter("eventTime")
     fun setEventTime(textView: TextView, date: Date?) {
-        val timeFormatter = SimpleDateFormat("hh:mm a zz", Locale.getDefault())
+        val timeFormatter = SimpleDateFormat(TIME_PATTERN, Locale.getDefault())
         if (date != null) {
             val formattedDate = timeFormatter.format(date)
             textView.text = formattedDate
@@ -25,7 +30,7 @@ object BindingAdapters {
     @JvmStatic
     @BindingAdapter("eventDate")
     fun setEventDate(textView: TextView, date: Date?) {
-        val timeFormatter = SimpleDateFormat("dd.M.Y", Locale.getDefault())
+        val timeFormatter = SimpleDateFormat(DATE_PATTERN, Locale.getDefault())
         if (date != null) {
             val formattedDate = timeFormatter.format(date)
             textView.text = formattedDate
@@ -35,12 +40,15 @@ object BindingAdapters {
     @JvmStatic
     @BindingAdapter("eventCoverImage")
     fun setEventCoverImage(imageView: ImageView, imageUrlList: List<Image>?) {
-        if (imageUrlList != null)
-            Picasso.with(imageView.context)
-                    .load(imageUrlList[1].big)
+        if (imageUrlList != null) {
+            Picasso.Builder(imageView.context)
+                    .downloader(OkHttp3Downloader(OkHttpClient()))
+                    .build().load(imageUrlList[FIRST_COVER_INDEX].big)
                     .placeholder(R.drawable.ic_placeholder_toast)
+                    .fit()
+                    .centerCrop()
                     .into(imageView)
+        }
     }
-
 
 }
