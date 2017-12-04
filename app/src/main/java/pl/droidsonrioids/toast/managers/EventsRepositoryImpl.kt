644747a80d7b64c056d313c9c9132d6ceba382ae
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2017-12-04 14:55:06 +0100

    [TOA-43] Delete loading events delay

--- app/src/main/java/pl/droidsonrioids/toast/managers/EventsRepositoryImpl.kt
+++ app/src/main/java/pl/droidsonrioids/toast/managers/EventsRepositoryImpl.kt
@@ -4,7 +4,6 @@ import io.reactivex.Maybe
 import pl.droidsonrioids.toast.data.model.Event
 import pl.droidsonrioids.toast.data.model.SplitEvents
 import pl.droidsonrioids.toast.services.EventService
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class EventsRepositoryImpl @Inject constructor(private val eventService: EventService) : EventsRepository {
@@ -16,7 +15,7 @@ class EventsRepositoryImpl @Inject constructor(private val eventService: EventSe
                             .firstOrNull()
                             ?.getSplitEventsMaybe(eventsResponse.events)
                             ?: Maybe.empty<SplitEvents>()
-                }.delay(2, TimeUnit.SECONDS)
+                }
     }
 
     private fun Event.getSplitEventsMaybe(previousEvents: List<Event>): Maybe<SplitEvents> {
