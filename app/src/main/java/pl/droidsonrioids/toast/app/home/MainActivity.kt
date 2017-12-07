58e1f79a77602e6e27cebc054adacc6a9747d5d9
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2017-12-07 14:41:31 +0100

    [TOA-20] WIP: Animate alpha of search view

--- app/src/main/java/pl/droidsonrioids/toast/app/home/MainActivity.kt
+++ app/src/main/java/pl/droidsonrioids/toast/app/home/MainActivity.kt
@@ -2,8 +2,10 @@ package pl.droidsonrioids.toast.app.home
 
 import android.os.Bundle
 import android.support.v4.view.ViewPager
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import pl.droidsonrioids.toast.R
 import pl.droidsonrioids.toast.app.base.BaseActivity
@@ -18,11 +20,18 @@ const val INFO_DIALOG_TAG = "info_dialog_tag"
 
 class MainActivity : BaseActivity() {
 
+    private val searchView by lazy {
+        findViewById<View>(R.id.menuItemSearch)
+    }
+
+    private var isMenuInflated = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupToolbar()
         setupViewPager()
+        Log.d("MENU", "ON_CREATE")
     }
 
     private fun setupToolbar() {
@@ -49,13 +58,44 @@ class MainActivity : BaseActivity() {
                 }
             }
 
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+                if (position == SPEAKERS_TAB_INDEX && positionOffset == 0f)
+                    setSearchIconVisible(1f)
+
+                if (position == SPEAKERS_TAB_INDEX && positionOffset > 0)
+                    setSearchIconVisible(1f - positionOffset)
+
+                if (position == EVENTS_TAB_INDEX && positionOffset > 0)
+                    setSearchIconVisible(positionOffset)
+
+                if (position == EVENTS_TAB_INDEX && positionOffset == 0f)
+                    setSearchIconVisible(0f)
+
+                if (position == CONTACT_TAB_INDEX && positionOffset == 0f)
+                    setSearchIconVisible(0f)
+            }
+
             override fun onPageScrollStateChanged(state: Int) {}
         })
     }
 
+    private fun setSearchIconVisible(alpha: Float) {
+        if (isMenuInflated) {
+            searchView.alpha = alpha
+//            searchView.translationY = -alpha *100
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        isMenuInflated = true
+        Log.d("MENU", "MENU_CREATED")
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        Log.d("MENU", "MENU_PREPARED ${findViewById<View>(R.id.menuItemSearch) == null}")
         return true
     }
 
--- app/src/main/java/pl/droidsonrioids/toast/app/speakers/SpeakersFragment.kt
+++ app/src/main/java/pl/droidsonrioids/toast/app/speakers/SpeakersFragment.kt
@@ -2,12 +2,13 @@ package pl.droidsonrioids.toast.app.speakers
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import pl.droidsonrioids.toast.R
 import pl.droidsonrioids.toast.app.base.BaseFragment
 
 class SpeakersFragment : BaseFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
             inflater.inflate(R.layout.fragment_speakers, container, false)
 }
