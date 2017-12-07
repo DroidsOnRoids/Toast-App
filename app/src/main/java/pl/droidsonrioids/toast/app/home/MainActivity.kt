aa9b80246e583a67215555fdc4608c324c0a661e
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2017-12-07 13:09:58 +0100

    [TOA-20] Change app title while tabs are scrolling

--- app/src/main/java/pl/droidsonrioids/toast/app/home/MainActivity.kt
+++ app/src/main/java/pl/droidsonrioids/toast/app/home/MainActivity.kt
@@ -1,14 +1,16 @@
 package pl.droidsonrioids.toast.app.home
 
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import pl.droidsonrioids.toast.R
 import pl.droidsonrioids.toast.app.base.BaseActivity
 
+
 const val EVENTS_TAB_INDEX = 0
-const val LECTURERS_TAB_INDEX = 1
+const val SPEAKERS_TAB_INDEX = 1
 const val CONTACT_TAB_INDEX = 2
 
 
@@ -33,9 +35,23 @@ class MainActivity : BaseActivity() {
         with(homeTabLayout) {
             setupWithViewPager(homeViewPager)
             getTabAt(EVENTS_TAB_INDEX)?.setIcon(R.drawable.ic_tab_events_selector)
-            getTabAt(LECTURERS_TAB_INDEX)?.setIcon(R.drawable.ic_tab_lecturers_selector)
+            getTabAt(SPEAKERS_TAB_INDEX)?.setIcon(R.drawable.ic_tab_lecturers_selector)
             getTabAt(CONTACT_TAB_INDEX)?.setIcon(R.drawable.ic_tab_contact_selector)
         }
+
+        homeViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    EVENTS_TAB_INDEX -> homeTitle.text = getString(R.string.events_title)
+                    SPEAKERS_TAB_INDEX -> homeTitle.text = getString(R.string.speakers_title)
+                    CONTACT_TAB_INDEX -> homeTitle.text = getString(R.string.toast_title)
+                }
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
