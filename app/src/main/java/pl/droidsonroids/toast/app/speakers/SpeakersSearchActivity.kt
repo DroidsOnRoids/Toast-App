6ab80b96151297d0432684b2a6d3ccd3761ff7f7
nonda <nonda95@gmail.com>
2017-12-15 12:55:52 +0100

    [TOA-38] Add speaker search bar

--- app/src/main/java/pl/droidsonroids/toast/app/speakers/SpeakersSearchActivity.kt
+++ app/src/main/java/pl/droidsonroids/toast/app/speakers/SpeakersSearchActivity.kt
@@ -2,10 +2,21 @@ package pl.droidsonroids.toast.app.speakers
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_speakers_search.*
+import pl.droidsonroids.toast.R
 import pl.droidsonroids.toast.app.base.BaseActivity
 
 class SpeakersSearchActivity : BaseActivity() {
     companion object {
         fun createIntent(context: Context): Intent = Intent(context, SpeakersSearchActivity::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speakers_search)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        searchBox.requestFocus()
     }
 }
