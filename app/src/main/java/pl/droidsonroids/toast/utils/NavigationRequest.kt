1567065b63a097d694b82045843c60965cb2da85
nonda <nonda95@gmail.com>
2017-12-20 10:17:05 +0100

    [TOA-44] Add navigation request for speaker details Add on click action for speakers list item

--- app/src/main/java/pl/droidsonroids/toast/utils/NavigationRequest.kt
+++ app/src/main/java/pl/droidsonroids/toast/utils/NavigationRequest.kt
@@ -2,5 +2,5 @@ package pl.droidsonroids.toast.utils
 
 sealed class NavigationRequest {
     object SpeakersSearch : NavigationRequest()
-    // TODO: TOA-44, TOA-57 Add other navigation requests
+    data class SpeakerDetails(val id: Long) : NavigationRequest()
 }
