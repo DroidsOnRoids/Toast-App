843de140052f34280eb85f8b8f9f678c641e156d
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2018-01-05 17:24:33 +0100

    [TOA-75] Add spinner

--- app/src/main/java/pl/droidsonroids/toast/app/contact/ContactFragment.kt
+++ app/src/main/java/pl/droidsonroids/toast/app/contact/ContactFragment.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.fragment_contact.*
+import pl.droidsonroids.toast.R
 import pl.droidsonroids.toast.app.base.BaseFragment
 import pl.droidsonroids.toast.databinding.FragmentContactBinding
 import pl.droidsonroids.toast.viewmodels.contact.ContactViewModel
+
 
 class ContactFragment : BaseFragment() {
 
@@ -24,6 +27,19 @@ class ContactFragment : BaseFragment() {
         val binding = FragmentContactBinding.inflate(inflater, container, false)
         binding.contactViewModel = contactViewModel
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupSpinner()
+    }
+
+    private fun setupSpinner() {
+        val adapter = ArrayAdapter.createFromResource(context, R.array.contact_topics, R.layout.item_contact_spinner)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        iWantToSpinner.adapter = adapter
     }
 
 }
