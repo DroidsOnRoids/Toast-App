11c4a447a603ecf551fd150e8583c8dac4a85308
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2018-01-05 15:23:34 +0100

    [TOA-75] Install contact fragment without spinner

--- app/src/main/java/pl/droidsonroids/toast/app/contact/ContactFragment.kt
+++ app/src/main/java/pl/droidsonroids/toast/app/contact/ContactFragment.kt
@@ -1,18 +1,29 @@
 package pl.droidsonroids.toast.app.contact
 
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import pl.droidsonroids.toast.R
+import pl.droidsonroids.toast.app.base.BaseFragment
+import pl.droidsonroids.toast.databinding.FragmentContactBinding
+import pl.droidsonroids.toast.viewmodels.contact.ContactViewModel
 
-class ContactFragment : Fragment() {
+class ContactFragment : BaseFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+    private lateinit var contactViewModel: ContactViewModel
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        contactViewModel = ViewModelProviders.of(this, viewModelFactory)[ContactViewModel::class.java]
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding = FragmentContactBinding.inflate(inflater, container, false)
+        binding.contactViewModel = contactViewModel
+        return binding.root
     }
 
 }
