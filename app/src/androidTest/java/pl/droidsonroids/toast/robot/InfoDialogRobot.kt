b2bbc1abf66ebac8177f544f812be651acb65159
tadychdorota <dorota.tadych@droidsonroids.pl>
2018-01-16 08:12:49 +0100

    fix test with Facebook deeplink using intent

--- app/src/androidTest/java/pl/droidsonroids/toast/robot/InfoDialogRobot.kt
+++ app/src/androidTest/java/pl/droidsonroids/toast/robot/InfoDialogRobot.kt
@@ -1,3 +1,21 @@
 package pl.droidsonroids.toast.robot
 
-class InfoDialogRobot : BaseRobot()
+import android.content.Intent
+import android.net.Uri
+import android.support.test.espresso.intent.Intents.intended
+import android.support.test.espresso.intent.matcher.IntentMatchers.hasAction
+import android.support.test.espresso.intent.matcher.IntentMatchers.hasData
+import android.support.test.espresso.intent.matcher.UriMatchers.hasHost
+import android.support.test.espresso.intent.matcher.UriMatchers.hasPath
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.equalTo
+import pl.droidsonroids.toast.R
+import pl.droidsonroids.toast.function.getString
+
+class InfoDialogRobot : BaseRobot() {
+    fun checkIfIntentedIntentOpensFacebook() {
+        intended(allOf(
+                hasAction(equalTo(Intent.ACTION_VIEW)),
+                hasData(Uri.parse(getString(R.string.toast_fanpage_url)))))
+    }
+}
