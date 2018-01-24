8650ec20ef8e2f82a363a3a39aa935b0d088e81c
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2018-01-24 10:17:48 +0100

    [TOA-46][WIP] Create layout

--- app/src/main/java/pl/droidsonroids/toast/utils/NavigationRequest.kt
+++ app/src/main/java/pl/droidsonroids/toast/utils/NavigationRequest.kt
@@ -7,9 +7,9 @@ import pl.droidsonroids.toast.data.enums.ParentView
 sealed class NavigationRequest {
     object SpeakersSearch : NavigationRequest()
     data class EventDetails(val id: Long) : NavigationRequest()
-    data class SpeakerDetails(val id: Long, val eventId: Long = Constants.NO_ID) : NavigationRequest()
+    data class SpeakerDetails(val id: Long) : NavigationRequest()
     object MessageSent : NavigationRequest()
-    data class TalkDetails(val talkDto: TalkDto, val eventId: Long) : NavigationRequest()
+    data class TalkDetails(val talkDto: TalkDto) : NavigationRequest()
     object Close : NavigationRequest()
     data class Photos(val photos: List<ImageDto>, val eventId: Long, val parentView: ParentView) : NavigationRequest()
     data class SinglePhoto(val photos: List<ImageDto>, val position: Long) : NavigationRequest()
