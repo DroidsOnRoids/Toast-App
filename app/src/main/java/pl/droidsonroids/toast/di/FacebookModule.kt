3ee1cf38579012b5bebc2503741963ea46a24f6d
nonda <nonda95@gmail.com>
2018-01-30 14:50:09 +0100

    Refactor

--- app/src/main/java/pl/droidsonroids/toast/di/FacebookModule.kt
+++ app/src/main/java/pl/droidsonroids/toast/di/FacebookModule.kt
@@ -4,6 +4,8 @@ import com.facebook.CallbackManager
 import com.facebook.login.LoginManager
 import dagger.Module
 import dagger.Provides
+import pl.droidsonroids.toast.app.login.FacebookLoginStateWatcher
+import pl.droidsonroids.toast.app.login.LoginStateWatcher
 import javax.inject.Singleton
 
 
@@ -12,11 +14,15 @@ class FacebookModule {
 
     @Singleton
     @Provides
-    fun provideLoginManager() = LoginManager.getInstance()
+    fun provideLoginManager(): LoginManager = LoginManager.getInstance()
 
     @Singleton
     @Provides
-    fun provideCallbackManager() = CallbackManager.Factory.create()
+    fun provideCallbackManager(): CallbackManager = CallbackManager.Factory.create()
+
+    @Singleton
+    @Provides
+    fun provideLoginStateWatcher(loginManager: LoginManager, loginCallbackManager: LoginCallbackManager): LoginStateWatcher = FacebookLoginStateWatcher(loginManager, loginCallbackManager)
 }
 
 typealias LoginCallbackManager = CallbackManager
--- app/src/main/java/pl/droidsonroids/toast/viewmodels/MainViewModel.kt
+++ app/src/main/java/pl/droidsonroids/toast/viewmodels/MainViewModel.kt
@@ -2,12 +2,11 @@ package pl.droidsonroids.toast.viewmodels
 
 import android.arch.lifecycle.ViewModel
 import io.reactivex.subjects.PublishSubject
-import pl.droidsonroids.toast.app.login.FacebookLoginStateWatcher
 import pl.droidsonroids.toast.app.login.LoginStateWatcher
 import pl.droidsonroids.toast.utils.NavigationRequest
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(facebookLoginStateWatcher: FacebookLoginStateWatcher) : ViewModel(), NavigatingViewModel, LoginStateWatcher by facebookLoginStateWatcher {
+class MainViewModel @Inject constructor(loginStateWatcher: LoginStateWatcher) : ViewModel(), NavigatingViewModel, LoginStateWatcher by loginStateWatcher {
     override val navigationSubject: PublishSubject<NavigationRequest> = PublishSubject.create()
 
     fun onSpeakerSearchRequested() {
