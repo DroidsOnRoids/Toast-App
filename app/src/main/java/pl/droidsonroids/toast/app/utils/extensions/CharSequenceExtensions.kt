9d431ffc22aed58de90e97e752fc788a4703cae3
nonda <nonda95@gmail.com>
2018-02-26 14:54:11 +0100

    [TOA-97] Add length filter for speaker search Refactor unicodeLength extension

--- app/src/main/java/pl/droidsonroids/toast/app/utils/extensions/CharSequenceExtensions.kt
+++ app/src/main/java/pl/droidsonroids/toast/app/utils/extensions/CharSequenceExtensions.kt
@@ -3,6 +3,6 @@
 package pl.droidsonroids.toast.app.utils.extensions
 
 
-fun CharSequence.getUnicodeLength()
-        = this.toString().codePointCount(0, this.length)
+val CharSequence.unicodeLength
+    get() = toString().codePointCount(0, length)
 
