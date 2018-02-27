8bee5569aab9994f867a12c3d209d7d7fb2a0b6c
Artyom Vlasov <artyom.vlasov@droidsonroids.pl>
2018-02-27 11:53:59 +0100

    [TOA-64] Refactor after CR

--- app/src/test/java/pl/droidsonroids/toast/rule/RxPluginSchedulerRule.kt
+++ app/src/test/java/pl/droidsonroids/toast/rule/RxPluginSchedulerRule.kt
@@ -6,13 +6,13 @@ import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.Schedulers
 import org.junit.rules.ExternalResource
 
-open class RxPluginSchedulerRule(private val computationScheduler: Scheduler = Schedulers.trampoline()) : ExternalResource() {
+open class RxPluginSchedulerRule(private val testScheduler: Scheduler = Schedulers.trampoline()) : ExternalResource() {
 
     override fun before() {
-        RxJavaPlugins.setComputationSchedulerHandler { computationScheduler }
-        RxJavaPlugins.setIoSchedulerHandler { computationScheduler }
-        RxJavaPlugins.setNewThreadSchedulerHandler { computationScheduler }
-        RxAndroidPlugins.setInitMainThreadSchedulerHandler { computationScheduler }
+        RxJavaPlugins.setComputationSchedulerHandler { testScheduler }
+        RxJavaPlugins.setIoSchedulerHandler { testScheduler }
+        RxJavaPlugins.setNewThreadSchedulerHandler { testScheduler }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { testScheduler }
     }
 
     override fun after() {
